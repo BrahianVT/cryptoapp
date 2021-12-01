9b3bd5388f7e7dac66810f9243ced84466c526bd
BrahianVT <pumasemj@hotmail.com>
2021-12-01 14:57:38 -0600

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ const App = () => {
             </Routes >
           </div>
         </Layout>
+
+
         <div className="footer" >
           <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
             Cryptoverse  <br />
