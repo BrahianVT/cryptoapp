d5ca401ae1bbdc4d83d7e45c5c09a84a96b39690
BrahianVT <pumasemj@hotmail.com>
2021-12-01 13:06:03 -0600

    added navbar and routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Router>
+    <App />
+  </Router>
+  , document.getElementById('root')
 );
 
