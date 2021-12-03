0fb6117a4ba3ebcdfc480ba5becec94d20dfa7f8
BrahianVT <pumasemj@hotmail.com>
2021-12-03 12:22:02 -0600

    added cryptoDetails

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -33,7 +33,7 @@ const Cryptocurrecies = ({ simplified }) => {
                 {
                     cryptos?.map((currency) => (
                         <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-                            <Link to={`/cryptos/${currency.id}`}>
+                            <Link to={`/crypto/${currency.id}`}>
                                 <Card title={`${currency.rank} . ${currency.name}`}
                                     extra={<img className="crypto-image" src={currency.iconUrl} />} hoverable>
                                     <p> Price: {millify(currency.price)}</p>
