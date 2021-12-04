774fd0bdd1bc61ef3c7d62507ddc15d4777431d5
BrahianVT <pumasemj@hotmail.com>
2021-12-03 18:22:48 -0600

    added responsive sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,33 @@ import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOut
 import icon from '../images/cryptocurrency.png';
 
 const Navbar = () => {
+    const [activeMenu, setActiveMenu] = React.useState(true);
+    const [screenSize, setScreenSize] = React.useState(null);
+
+    React.useEffect( () =>{
+        const handleResize = () => setScreenSize(window.innerWidth);
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    },[]);
+
+    React.useEffect( () =>{
+       setActiveMenu(screenSize < 768?false: true);
+    },[screenSize]);
+
     return (
         <div className="nav-container">
             <div className="logo-container">
                 <Avatar src={icon} size="small" />
                 <Typography.Title level={2} className="logo"> <Link to="/"> Criptoverse</Link></Typography.Title>
-                {/* <Button className="menu-control-container"> </Button> */}
+                 <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}> 
+                    <MenuOutlined/>
+                 </Button>
             </div>
-            <Menu theme="dark">
+            {activeMenu && (
+                <Menu theme="dark">
                 <Menu.Item icon={<HomeOutlined />} >
                     <Link to="/" >Home</Link>
                 </Menu.Item>
@@ -27,6 +46,7 @@ const Navbar = () => {
                     <Link to="/news" >News</Link>
                 </Menu.Item>
             </Menu>
+            )}
 
         </div>
     )
